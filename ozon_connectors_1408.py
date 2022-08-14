dc427f6dce0fb909d2dabd8353d074890af27f1e
kbzunder <kbzunder@gmail.com>
2022-08-14 16:32:05 +0300

    rewritten

--- ozon_connectors_1408.py
+++ ozon_connectors_1408.py
@@ -45,29 +45,55 @@ if __name__ == '__main__':
 #     } 
 
 ## returns dataframes ready to bigquery 
+    headers={
+        'Client-Id':'42918', 
+        'Api-Key':'edbfc6b6-e0fc-446d-9c5d-454acf372925'
+    } 
 
-    def get_data(date,json,headers,url):
-        if 'date_from' in json:
-            json["date_from"] = date
-        elif 'from' in json['date']:
-            json['date']['from'] = date + 'T00:00:00.000Z'
-
+    def get_data(json,headers,url):
         req = requests.post(url, headers=headers, json=json)
         if req.status_code == 200:
             return req.json()
         else:
             raise ValueError
 
-    def get_costs(params):
-        j = get_data(params)
+    def get_costs(date):
+        json={
+     "date_from": date,
+     "date_to": date,
+     "metrics": [
+     "adv_sum_all"
+     ],
+   "dimension": [
+     "day",
+       "month",
+      "year"
+   ],
+     "filters": [],
+     "sort": [
+   ],
+    "limit": 1000,
+    "offset": 0
+    }
+        url = 'https://api-seller.ozon.ru/v1/analytics/data'
+        j = get_data(json, headers, url)
         df_cost = pd.DataFrame.from_dict(j) 
         key = df_cost['result']['data'][0]['dimensions'][0]['id']
         value = df_cost['result']['totals'][0]
         data_cost = pd.DataFrame({'date':[key],'source':['partner.ozon.ru'],'medium':['referral'],'campaign':[" "],'Cost':[value]})
         return data_cost
 
-    def get_compensation(params):
-        j = get_data(params)
+    def get_compensation(date):
+        json={
+             "date": {
+           "from": date + 'T00:00:00.000Z',
+            "to": date + 'T00:00:00.000Z'
+            },
+            "posting_number": "",
+             "transaction_type": "all"
+           }
+        url = 'https://api-seller.ozon.ru/v3/finance/transaction/totals'
+        j = get_data(json, headers, url)
         data_comp =    pd.DataFrame.from_dict(j).T.reset_index(drop=True)
         data_comp['Cost'] = abs(data_comp.drop('accruals_for_sale', axis=1).sum(axis=1))
         data_comp['date'] = date
@@ -87,12 +113,12 @@ if __name__ == '__main__':
         end = datetime.strptime(end, '%Y-%m-%d').date()
         date_range_datetime(start, end)
 
-    def data_to_gbq(start, end, params):
+    def data_to_gbq(start, end):
 
         for day in date_range_string(start,end):
             
-            df1 = get_costs(params)
-            df2 = get_compensation(params)
+            df1 = get_costs(day)
+            df2 = get_compensation(day)
 
             df1.to_gbq('Costs.HOLOWAY_OZON_Costs_compensation',
                 ##chunksize=None, # I have tried with several chunk sizes, it runs faster when it's one big chunk (at least for me)
